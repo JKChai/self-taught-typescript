1d0ca6cf2e3ac59b28b8986dbf8fd57423e38202
JKChai <jk.chai@okstate.edu>
2021-12-30 22:38:55 -0600

    Update file

---
+++ type.ts
@@ -0,0 +1,33 @@
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: "Maximillian",
+//     age: 30,
+//     hobbies: ['sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+enum Role { ADMIN, READ_ONLY, AUTHOR, }
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: "Maximillian",
+    age: 30,
+    hobbies: ['sports', 'Cooking'],
+    role: Role.ADMIN
+}
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.age)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby)
+}
